3bf154090deeeea5c478186288e1d78e06e1fcc2
SoufianeSalama <soufisalama@hotmail.com>
2018-12-13 13:51:53 +0100

    basic helloworld

---
+++ webserver_cowboy/src/helloworld_handler.erl
@@ -0,0 +1,14 @@
+-module(helloworld_handler).
+-behavior(cowboy_handler).
+
+-export([init/2]).
+
+%%init(Req, State) ->
+%%	{ok, Req, State}.
+
+init(Req0, State) ->
+	Req = cowboy_req:reply(200,
+		#{<<"content-type">> => <<"text/plain">>},
+		<<"Hello World">>,
+		Req0),
+	{ok, Req, State}.

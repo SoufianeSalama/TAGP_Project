7f8ff916869a262957769896a9d9e3a7207054d6
SoufianeSalama <soufisalama@hotmail.com>
2018-12-18 22:52:28 +0100

    Deeper Data Binding (Device)

--- webserver_cowboy/src/devices_handler.erl
+++ webserver_cowboy/src/devices_handler.erl
@@ -4,14 +4,31 @@
 -export([init/2]).
 
 init(Req0, State) ->
+	%Devicestable = ets:new(devicestable, []),
+	%ets:insert(Devicestable, [{Verwarming}],
+	%ets:insert(Devicestable, [{Oven}],
+	%ets:insert(Devicestable, [{Pomp}],
+	%ets:insert(Devicestable, [{Tuinlichten}],
 	{ok, Body} = devices_dtl:render([
 		{template_name, "Devices Manager"},
-		{devices, [
-			<<"Verwarming">>,
-			<<"Oven">>,
-			<<"Pomp">>,
-			<<"Buitenlichten">>
-			]}
+		
+		{devices, 
+			[
+				[
+				{device_name, "pomp"},
+				{device_description, "..."},
+				{device_location, "buiten"},
+				{device_status, "1"}
+				],
+
+				[
+				{device_name, "verwarming"},
+				{device_description, "..."},
+				{device_location, "binnen"},
+				{device_status, "1"}
+				]
+			]
+		}
 		]),
 	{ok, Req2} = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>}, Body, Req0),	
 	{ok, Req2, State}.
@@ -20,3 +37,12 @@ init(Req0, State) ->
 %init(Req0, State) ->
 %	erlydtl:compile("./priv/smurfin.html", smurfin_template),
 %	smurfin_template:rend
+
+
+%{devices, [
+		%	"Verwarmingennnnn",
+		%	"Oven",
+		%	"Pomp",
+		%	"Tuinlichten"
+		%	]}
+		%]),
